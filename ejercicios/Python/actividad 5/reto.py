e55a1dfc5a051495d4b8744e30b75430905009a7
jramale988 <josemanuelramirezalegre@gmail.com>
2024-11-20 12:32:36 +0100

    HTML

--- ejercicios/Python/actividad 5/reto.py
+++
@@ -1,8 +0,0 @@
-cadena = "Esta clase de 2SMRA va a hacer huelga"
-n=0
-for letra in cadena:
-    if letra==" ":
-        n=n+1
-npal = n+1
-
-print (npal)
--- ejercicios/Python/actividad 5/sdf.py
+++
@@ -1,6 +0,0 @@
-anchura = int(input("Dame la anchura: "))
-
-for x in range(anchura):
-    print ("* "*(x+1))
-for x in range(anchura,0,-1):
-    print ("* "*(x-1))
