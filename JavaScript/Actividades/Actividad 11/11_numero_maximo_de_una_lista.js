ec399070d3d53f3b70725001b67a3e93fbfbbdfb
jramale988 <josemanuelramirezalegre@gmail.com>
2025-02-03 11:52:23 +0100

    estructura proyecto

--- JavaScript/Actividades/Actividad 11/11_numero_maximo_de_una_lista.js
+++ JavaScript/Actividades/Actividad 11/11_numero_maximo_de_una_lista.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-
+    //Funcion
     function findLargestNumber (numeros) {
         let mayor = numeros[0];
-        for (let n = 1; n < numeros.length; n++) {
-            if (numeros[n] > mayor) {
-                mayor = numeros[n];
+        for (let n = 1; i < numeros.length; i++) {
+            if (numeros[i] > mayor) {
+                mayor = numeros[i];
             }
         }
         return mayor;
@@ -13,14 +13,13 @@ document.addEventListener("DOMContentLoaded", function(){
     
     
     
-    let Cantidad_N = parseInt(prompt("Ingresa la cantidad de números: "));
-    
-    
-    
+
+    // Parte principal
+    const Cantidad_I = parseInt(prompt("Ingresa la cantidad de números: "));
     let lista = [];
     
     
-    for (n = 1; n <= Cantidad_N; n++) {
+    for (n = 1; n <= Cantidad_I; n++) {
         numero = prompt("Ingresa el número " + (n));
         lista.push(numero);
     }
